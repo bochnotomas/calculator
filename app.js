b9e1d193eee07347922a47a5bab32a7e059951bc
bochnotomas <tomasbochnovic76@gmail.com>
2021-03-05 17:42:32 +0000

    Whole functionality of a basic calculator finished, redesign occured as well

--- app.js
+++ app.js
@@ -5,12 +5,19 @@ const symbols = document.querySelectorAll(".calculator .symbol")
 let values = [" ", " "]
 let operation = ""
 let errorFlag = false
+let step = false
 
 //Event listener for number 1-9, brackets and the dot
 numbers.forEach(number => {
     number.addEventListener('click', () => {
-        display.innerText += number.innerText
+
+        if (display.innerText.length > 11) {
+            alert("Lenght of 12 numbers is a maximum!")
+        } else {
+            display.innerText += number.innerText
+        }
     })
+
 })
 
 //Event listener for symbols +,-,/,*,=
@@ -18,28 +25,49 @@ symbols.forEach(symbol => {
     symbol.addEventListener('click', () => {
         switch (symbol.innerText) {
             case '+':
+                if (step === true) {
+                    values[1] = display.innerText
+                    operate(operation, values[0], values[1])
+                }
                 operation = symbol.innerText
                 values[0] = display.innerText
                 display.innerHTML = ""
+                step = true
                 break;
             case '−':
+                if (step === true) {
+                    values[1] = display.innerText
+                    operate(operation, values[0], values[1])
+                }
                 operation = symbol.innerText
                 values[0] = display.innerText
                 display.innerHTML = ""
+                step = true
                 break;
             case '×':
+                if (step === true) {
+                    values[1] = display.innerText
+                    operate(operation, values[0], values[1])
+                }
                 operation = symbol.innerText
                 values[0] = display.innerText
                 display.innerHTML = ""
+                step = true
                 break;
             case '/':
+                if (step === true) {
+                    values[1] = display.innerText
+                    operate(operation, values[0], values[1])
+                }
                 operation = symbol.innerText
                 values[0] = display.innerText
                 display.innerHTML = ""
+                step = true
                 break;
             case '=':
                 values[1] = display.innerText
                 operate(operation, values[0], values[1])
+                step = false
                 break;
             case '→':
                 del()
@@ -51,6 +79,7 @@ symbols.forEach(symbol => {
     })
 })
 
+//operations functions
 function add(number1, number2) {
     let sum = parseFloat(number1) + parseFloat(number2)
     values[0] = sum
@@ -80,7 +109,7 @@ function divide(number1, number2) {
     }
 }
 
-
+//Main function which handles operation
 function operate(operator, number1, number2) {
     let dotAmount1 = number1.split('.').length - 1
     let dotAmount2 = number2.split('.').length - 1
@@ -109,12 +138,20 @@ function operate(operator, number1, number2) {
             break;
     }
 
+    if (display.innerText.length > 11 && dotAmount1 > 1) {
+        display.innerText = round(parseFloat(display.innerText))
+    } else if (display.innerText.length > 11 && dotAmount1 < 1) {
+        display.innerText = exp(parseFloat(display.innerText))
+    }
+
     if (errorFlag == true) {
         clear()
     }
+
+
 }
 
-
+//clear function which resets the whole calculator
 function clear() {
     display.innerHTML = ""
     operation = ""
@@ -122,8 +159,15 @@ function clear() {
     errorFlag = false
 }
 
-
+//deleting one character
 function del() {
-    display.innerText = display.innerText.substring(1);
+    display.innerText = display.innerText.slice(0, -1)
 }
 
+function round(number) {
+    return number.toFixed(4);
+}
+
+function exp(number) {
+    return number.toExponential(4);
+}
